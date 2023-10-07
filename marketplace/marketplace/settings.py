aefa1ba0a1990e121e671d9b1362de65ae16f49e
maurixavi <mxavierarevalo@gmail.com>
2023-10-07 03:31:29 -0300

    Add index view

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-_b8ce46i)7_wfawn#-9&0t-e(^a(ya7f%th4%d54=+__he!i=z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
-
+#ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
